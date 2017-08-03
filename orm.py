0218c3ed321c23df6aeaf50b8d61f9813f534428
stanJ <cjiao@3ti.us>
2017-08-03 17:10:08 +0800

    KeyError

--- orm.py
+++ orm.py
@@ -31,7 +31,7 @@ class Model(dict, metaclass=ModelMetaclass):
     def __getattr__(self, key):
         try:
             return self[key]
-        except keyError:
+        except KeyError:
             raise AttributeError(r"'Model' object has no attribute '%s'" % key)
     def __setattr__(self, key, value):
         self[key] = value
