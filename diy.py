6038f9b1688cf4c8b04a4828323f9224ddc293e6
stanJ <cjiao@3ti.us>
2017-08-03 16:46:21 +0800

    17-08-03

--- diy.py
+++ diy.py
@@ -1,6 +1,14 @@
 class Student(object):
     def __init__(self, name):
         self.name = name
+    def __getattr__(self, attr):
+        if attr == 'score':
+            return 99
+        if attr == 'age':
+            return lambda: 25
+        raise AttributeError('\'Student\' object has no attribute \'%s\'' % attr)
+    def __call__(self):
+        print('My name is %s.' % self.name)
     def __str__(self):
         return 'Student object (name: %s)' % self.name
     __repr__ = __str__
@@ -15,7 +23,31 @@ class Fib(object):
             raise StopIteration()
         return self.a
     def __getitem__(self, n):
-        a, b = 1, 1
-        for x in range(n):
-            a, b = b, a + b
-        return a
+        if isinstance(n, int):
+            a, b = 1, 1
+            for x in range(n):
+                a, b = b, a + b
+            return a
+        if isinstance(n, slice):
+            start = n.start
+            stop = n.stop
+            if start is None:
+                start = 0
+            a, b = 1, 1
+            L = []
+            for x in range(stop):
+                if x >= start:
+                    L.append(a)
+                a, b = b, a + b
+            return L
+
+class Chain(object):
+    def __init__(self, path=''):
+        self._path = path
+    def __getattr__(self, path):
+        if path == 'users':
+            return lambda x: Chain('%s/users/%s' % (self._path, x))
+        return Chain('%s/%s' % (self._path, path))
+    def __str__(self):
+        return self._path
+    __repr__ = __str__
