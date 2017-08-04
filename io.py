bccec957c3baeb4b60a8d3722e1909b4346e3d56
stanJ <cjiao@3ti.us>
2017-08-04 18:09:18 +0800

    io

---
+++ io.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import logging
+# try:
+#     f = open('aa.txt', 'r', encoding='utf-8')
+#     print(f.read())
+# except FileNotFoundError as e:
+#     logging.exception(e)
+# finally:
+#     if f:
+#         f.close()
+
+# with open('test.txt', 'r', encoding='utf-8') as f:
+#     for line in f.readlines():
+#         print(line.strip())
+
+# with open('群体.jpg', 'rb') as f:
+#     print(f.read())
+
+with open('test.txt', 'w') as f:
+    f.write('Hello, world!')
