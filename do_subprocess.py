b9b85a13aee9d7680344ba83261c462ca3084d27
stanJ <cjiao@3ti.us>
2017-08-08 18:39:43 +0800

    17-08-08

--- do_subprocess.py
+++ do_subprocess.py
@@ -3,9 +3,9 @@
 
 import subprocess
 
-print('$ nslookup www.python.org')
-r = subprocess.call(['nslookup', 'www.python.org'])
-print('Exit code:', r)
+# print('$ nslookup www.python.org')
+# r = subprocess.call(['nslookup', 'www.python.org'])
+# print('Exit code:', r)
 
 print('$ nslookup')
 p = subprocess.Popen(['nslookup'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
